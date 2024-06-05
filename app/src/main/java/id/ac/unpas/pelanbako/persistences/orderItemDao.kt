c7dd3635bdc0d4b5d95d34a0617525a344fe50bb
Ayrus16 <suryap.sp83@gmail.com>
2024-06-05 14:31:03 +0700

    repair order

--- app/src/main/java/id/ac/unpas/pelanbako/persistences/orderItemDao.kt
+++ app/src/main/java/id/ac/unpas/pelanbako/persistences/orderItemDao.kt
@@ -6,17 +6,20 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import id.ac.unpas.pelanbako.models.item
+import id.ac.unpas.pelanbako.models.orderItem
+
 interface orderItemDao {
     @Insert
-    suspend fun insertOrderItem(orderItem: OrderItem)
+    suspend fun insertOrderItem(orderItem: orderItem)
 
     @Update
-    suspend fun updateOrderItem(orderItem: OrderItem)
+    suspend fun updateOrderItem(orderItem: orderItem)
 
     @Delete
-    suspend fun deleteOrderItem(orderItem: OrderItem)
+    suspend fun deleteOrderItem(orderItem: orderItem)
 
     @Query("SELECT * FROM order_item WHERE order_id = :orderId")
-    suspend fun getOrderItemsByOrderId(orderId: Int): List<OrderItem>
+    suspend fun getOrderItemsByOrderId(orderId: Int): List<orderItem>
 }
